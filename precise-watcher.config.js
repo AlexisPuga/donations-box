fb7794b209c6658e3c9337f28fa64d31d18aff2f
Alexis Puga <alexispuga@pm.me>
2021-06-20 02:09:53 -0500

    ENH: Run "nunjucks-to-html" in precise-watcher.config.js
    Listen for the "ready" and "change" events.

--- precise-watcher.config.js
+++ precise-watcher.config.js
@@ -6,5 +6,12 @@ module.exports = {
 			'cmd': 'serve',
 			'args': ['public'],
 		}]
+	}, {
+		'pattern': ['static/**/*'],
+		'on': ['ready', 'change'],
+		'run': [{
+			'cmd': 'nunjucks-to-html',
+			'args': ['**/*', '--baseDir', 'static']
+		}]
 	}]
 };
