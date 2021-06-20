bb7ed794c9b664b0bd32b6b23e5577b673f71657
Alexis Puga <alexispuga@pm.me>
2021-06-20 16:36:43 -0500

    BUG: Fix EISDIR error. Run nunjucks-to-html only on /static/*.njk files

--- precise-watcher.config.js
+++ precise-watcher.config.js
@@ -11,7 +11,7 @@ module.exports = {
 		'on': ['ready', 'change'],
 		'run': [{
 			'cmd': 'nunjucks-to-html',
-			'args': ['**/*', '--baseDir', 'static']
+			'args': ['*.njk', '--baseDir', 'static']
 		}]
 	}]
 };
