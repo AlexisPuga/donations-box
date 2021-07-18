3043aadabb0aa99fd8c39a96874de9dde6ce2624
Alexis Puga <alexispuga@pm.me>
2021-07-18 05:11:15 -0500

    API: Use donation.via as the donations' property key
    The "donations" property is now an object which contains payment providers
    (previously called donation.via) as keys and its donations as values.
    This allows us to easily show payment providers before its corresponding
    donations.
    
    DEP: Remove donation.via property
    
    MAINT: Replace #donations <ol> with <div>
    
    MAINT: Describe each list of donations using its paymentProvider
    
    Use a <p> to show the paymentProvider variable.
    
    MAINT: Create multiple <ol> for each paymentProvider

--- nunjucks.config.js
+++ nunjucks.config.js
@@ -19,70 +19,73 @@ module.exports = {
 			'size': 'small',
 			// This donations are examples.
 			// You can modify them any way you want.
-			'donations': [{
-				'type': 'one-off',
-				'href': '/donate.html',
-				'title': 'Buy me 1 coffee',
-				'amounts': [{
-					'value': 3,
-					'currency': dollars
-				}]
-			}, {
-				'type': 'one-off',
-				'href': '/donate.html',
-				'title': 'Buy me 2 coffees',
-				'amounts': [{
-					'value': 6,
-					'currency': dollars
-				}]
-			}, {
-				'type': 'one-off',
-				'href': '/donate.html',
-				'title': 'Buy me 3 coffees',
-				'amounts': [{
-					'value': 9,
-					'currency': dollars
-				}]
-			}, {
-				'type': 'recurrent',
-				'title': 'Buy me a daily coffee',
-				'amounts': [{
+			'donations': {
+				'Stripe': [{
+					'type': 'one-off',
 					'href': '/donate.html',
-					'value': 3,
-					'periodicity': 'Daily',
-					'currency': dollars
+					'title': 'Buy me 1 coffee',
+					'amounts': [{
+						'value': 3,
+						'currency': dollars
+					}]
 				}, {
+					'type': 'one-off',
 					'href': '/donate.html',
-					'value': 3 * 7,
-					'periodicity': 'Weekly',
-					'currency': dollars
+					'title': 'Buy me 2 coffees',
+					'amounts': [{
+						'value': 6,
+						'currency': dollars
+					}]
 				}, {
+					'type': 'one-off',
 					'href': '/donate.html',
-					'value': 3 * 30,
-					'periodicity': 'Monthly',
-					'currency': dollars
+					'title': 'Buy me 3 coffees',
+					'amounts': [{
+						'value': 9,
+						'currency': dollars
+					}]
 				}, {
-					'href': '/donate.html',
-					'value': 3 * 365,
-					'periodicity': 'Yearly',
-					'currency': dollars
+					'type': 'recurrent',
+					'title': 'Buy me a daily coffee',
+					'amounts': [{
+						'href': '/donate.html',
+						'value': 3,
+						'periodicity': 'Daily',
+						'currency': dollars
+					}, {
+						'href': '/donate.html',
+						'value': 3 * 7,
+						'periodicity': 'Weekly',
+						'currency': dollars
+					}, {
+						'href': '/donate.html',
+						'value': 3 * 30,
+						'periodicity': 'Monthly',
+						'currency': dollars
+					}, {
+						'href': '/donate.html',
+						'value': 3 * 365,
+						'periodicity': 'Yearly',
+						'currency': dollars
+					}]
+				}],
+				'Monero': [{
+					'type': 'one-off',
+					'title': 'Donate via Monero',
+					'amounts': [{
+						'href': 'monero:8Bqeu7beB2ocwTr1Gn4gz6VUUoqVDokiR9WAY2E8m5toNSbRvRMrWjgWYnaNGchWc2cVRgVvidNpLjZF5vXJnauf7s2oHPw',
+						'qr': {
+							'src': '/img/monero-qr.png',
+							'alt': 'Donation Box\'s Monero QR Code',
+							'srcset': '/img/monero-qr.png 2x'
+						},
+						'currency': {
+							'name': 'Monero',
+							'abbr': 'XMR'
+						}
+					}]
 				}]
-			}, {
-				'type': 'one-off',
-				'title': 'Donate via Monero',
-				'amounts': [{
-					'href': 'monero:8Bqeu7beB2ocwTr1Gn4gz6VUUoqVDokiR9WAY2E8m5toNSbRvRMrWjgWYnaNGchWc2cVRgVvidNpLjZF5vXJnauf7s2oHPw',
-					'qr': {
-						'src': '/img/monero-qr.png',
-						'alt': 'Donation Box\'s Monero QR Code',
-						'srcset': '/img/monero-qr.png 2x'
-					},
-					'currency': {
-						'name': 'Monero',
-						'abbr': 'XMR'
-					}
-				}]
-			}]
+			}
 		}
 	}
 };
