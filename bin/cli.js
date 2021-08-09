d42fb2a401c6703cf4d87582832d6712b3775458
Alexis Puga <alexispuga@pm.me>
2021-08-09 08:47:06 -0500

    ENH: Support markup injection via CLI
    This commit adds support to inject donation box's markup in the given files,
    using the bin/cli.js file.
    
    Features:
    * Multiple files and globs are supported.
    * Errors, stdout, and stderr are logged.
    * Replacement is done in the same file.

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+const {exec} = require('child_process');
+const fs = require('fs');
 const yargs = require('yargs/yargs');
+const glob = require('glob');
 const {argv} = yargs(process.argv.slice(2))
 	.demandCommand(1)
 	.usage('$0 <files...> [options]', 'Inject the donations box\'s markup in the given files.', (yargs) => yargs
@@ -11,3 +14,32 @@ const {argv} = yargs(process.argv.slice(2))
 			'default': '<!-- donations-box -->'
 		})
 	);
+const {token, files} = argv;
+const markupFilepath = 'dist/donations-box.html';
+
+exec('npm run build', (error, stdout, stderr) => {
+
+	if (error) { return void console.error('[donations-box] error:', error); }
+	if (stderr) { console.error('[donations-box] stderr:', stderr); }
+	if (stdout) { console.log('[donations-box]', stdout); }
+
+	const markup = fs.readFileSync(markupFilepath);
+
+	glob(files, (error, filepaths) => {
+
+		if (error) { return void console.error('[donations-box] error:', error); }
+
+		filepaths.forEach((filepath) => {
+
+			const file = fs.readFileSync(filepath).toString();
+			const replacedFile = file.replace(token, markup);
+			/** @TODO Allow replacing in another file. I.e: Prevent overrides. */
+			const outFile = filepath;
+
+			fs.writeFileSync(outFile, replacedFile);
+
+		});
+
+	});
+
+});
