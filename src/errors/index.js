4abac670a2ce0e02edd46f6180e851aa135cdba2
Alexis Puga <alexispuga@pm.me>
2021-08-09 05:23:22 -0500

    ENH: Index errors
    Add src/errors/index.js
    Export build-error, injection-error, and copy-error.

---
+++ src/errors/index.js
@@ -0,0 +1,9 @@
+const InjectionError = require('./injection-error');
+const BuildError = require('./build-error');
+const CopyError = require('./copy-error');
+
+module.exports = {
+	InjectionError,
+	BuildError,
+	CopyError
+};
