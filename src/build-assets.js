d4465e6e8e9eff92e6d1202ca9f14c27fa21f6a7
Alexis Puga <alexispuga@pm.me>
2021-08-09 06:53:12 -0500

    BUG: Fix. Run build command from project root
    Set cwd to project root.
    Set config file relative to users location (cwd).

--- src/build-assets.js
+++ src/build-assets.js
@@ -1,12 +1,16 @@
 const {exec} = require('child_process');
+const path = require('path');
+const PROJECT_ROOT = path.resolve(__dirname, '..');
 
 module.exports = ({config}) => new Promise((resolve, reject) => {
 
 	exec('npm run build', {
+		'cwd': PROJECT_ROOT,
 		'env': {
 			...process.env,
 			// Send this variable to nunjucks-to-html command within the build script.
-			'DONATIONS_BOX_CONFIG_FILE': config
+			// Make sure to set it relative to cwd.
+			'DONATIONS_BOX_CONFIG_FILE': path.resolve(process.cwd(), config)
 		}
 	}, (error, stdout, stderr) => {
 
