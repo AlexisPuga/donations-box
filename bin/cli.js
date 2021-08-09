1f1818b2b8eafdf07d889b348fcb2549e11fd620
Alexis Puga <alexispuga@pm.me>
2021-08-09 03:00:27 -0500

    MAINT: Move non-CLI code from bin/cli.js to src/distribute.js
    Import src/distribute.js from bin/cli.js and call it with the argv options instead.
    
    ENH: Add src/distribute.js
    
    Copy non-CLI related code from bin/cli.js.
    Wrap it in a function.
    Set yargs#argv the params of the function.
    Export that function.

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-const {exec} = require('child_process');
-const fs = require('fs');
-const path = require('path');
 const yargs = require('yargs/yargs');
-const glob = require('glob');
+const distribute = require('../src/distribute');
 const {argv} = yargs(process.argv.slice(2))
 	.demandCommand(1)
 	.usage('$0 <files...> [options]', 'Inject the donations box\'s markup in the given files.', (yargs) => yargs
@@ -23,73 +20,5 @@ const {argv} = yargs(process.argv.slice(2))
 		})
 		.demandOption('config', 'Please provide the path to your configuration file using --config [filepath].')
 	);
-const {token, files, config, assetsDir} = argv;
-const destinationDir = path.resolve('dist');
-const markupFilepath = path.resolve(destinationDir, 'donations-box.html');
-const assetsFilepath = path.resolve(destinationDir, '**', '*.css');
-const eachFilepath = (files, fn) => {
 
-	const filepaths = glob.sync(files);
-
-	filepaths.forEach(fn);
-
-};
-const ensureDirectory = (filepath) => {
-
-	const directory = path.dirname(filepath);
-
-	if (!fs.existsSync(directory)) {
-
-		fs.mkdirSync(directory, {
-			'recursive': true
-		});
-
-	}
-
-};
-
-exec('npm run build', {
-	'env': {
-		...process.env,
-		// Send this variable to nunjucks-to-html command within the build script.
-		'DONATIONS_BOX_CONFIG_FILE': config
-	}
-}, (error, stdout, stderr) => {
-
-	if (error) { return void console.error('[donations-box] error:', error); }
-	if (stderr) { console.error('[donations-box] stderr:', stderr); }
-	if (stdout) { console.log('[donations-box]', stdout); }
-
-	const markup = fs.readFileSync(markupFilepath);
-
-	try {
-
-		eachFilepath(files, (filepath) => {
-
-			const file = fs.readFileSync(filepath).toString();
-			const replacedFile = file.replace(token, markup);
-			/** @TODO Allow replacing in another file. I.e: Prevent overrides. */
-			const outFile = filepath;
-
-			fs.writeFileSync(outFile, replacedFile);
-
-		});
-
-	}
-	catch (error) { return void console.error('[donations-box] Error:', error); }
-
-	try {
-
-		eachFilepath(assetsFilepath, (filepath) => {
-
-			const destinationFilepath = filepath.replace(destinationDir, assetsDir);
-
-			ensureDirectory(destinationFilepath);
-			fs.copyFileSync(filepath, destinationFilepath);
-
-		});
-
-	}
-	catch (error) { return void console.error('[donations-box] Error while copying assets:', error); }
-
-});
+distribute(argv);
---
+++ src/distribute.js
@@ -0,0 +1,78 @@
+const {exec} = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+module.exports = ({token, files, config, assetsDir}) => {
+
+	const destinationDir = path.resolve('dist');
+	const markupFilepath = path.resolve(destinationDir, 'donations-box.html');
+	const assetsFilepath = path.resolve(destinationDir, '**', '*.css');
+	const eachFilepath = (files, fn) => {
+
+		const filepaths = glob.sync(files);
+
+		filepaths.forEach(fn);
+
+	};
+	const ensureDirectory = (filepath) => {
+
+		const directory = path.dirname(filepath);
+
+		if (!fs.existsSync(directory)) {
+
+			fs.mkdirSync(directory, {
+				'recursive': true
+			});
+
+		}
+
+	};
+
+	exec('npm run build', {
+		'env': {
+			...process.env,
+			// Send this variable to nunjucks-to-html command within the build script.
+			'DONATIONS_BOX_CONFIG_FILE': config
+		}
+	}, (error, stdout, stderr) => {
+
+		if (error) { return void console.error('[donations-box] error:', error); }
+		if (stderr) { console.error('[donations-box] stderr:', stderr); }
+		if (stdout) { console.log('[donations-box]', stdout); }
+
+		const markup = fs.readFileSync(markupFilepath);
+
+		try {
+
+			eachFilepath(files, (filepath) => {
+
+				const file = fs.readFileSync(filepath).toString();
+				const replacedFile = file.replace(token, markup);
+				/** @TODO Allow replacing in another file. I.e: Prevent overrides. */
+				const outFile = filepath;
+
+				fs.writeFileSync(outFile, replacedFile);
+
+			});
+
+		}
+		catch (error) { return void console.error('[donations-box] Error:', error); }
+
+		try {
+
+			eachFilepath(assetsFilepath, (filepath) => {
+
+				const destinationFilepath = filepath.replace(destinationDir, assetsDir);
+
+				ensureDirectory(destinationFilepath);
+				fs.copyFileSync(filepath, destinationFilepath);
+
+			});
+
+		}
+		catch (error) { return void console.error('[donations-box] Error while copying assets:', error); }
+
+	});
+
+};
