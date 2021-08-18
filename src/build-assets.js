5cabe03bfa103122479ebb6fc89e2de9b2a6878d
Alexis Puga <alexispuga@pm.me>
2021-08-18 08:32:01 -0500

    ENH: Cache build
    Update src/build-assets.js:
    - Copy the user's config file into /dist:
      - If an exception occurs, log it using console.error.
    - Check if the stored config file is the same as the user's one:
      - If so, resolve.
      - Else, build as normal.

--- src/build-assets.js
+++ src/build-assets.js
@@ -1,8 +1,16 @@
 const {exec} = require('child_process');
 const path = require('path');
+const fs = require('fs');
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 
 module.exports = ({config}) => new Promise((resolve, reject) => {
+
+	const configFilepath = path.resolve(process.cwd(), config);
+	const cachedConfigFilepath = path.resolve(PROJECT_ROOT, 'dist', config);
+	const cached = fs.existsSync(cachedConfigFilepath)
+		&& fs.readFileSync(configFilepath, 'utf8') === fs.readFileSync(cachedConfigFilepath, 'utf8');
+
+	if (cached) { return void resolve(); }
 
 	exec('npm run build', {
 		'cwd': PROJECT_ROOT,
@@ -10,13 +18,17 @@ module.exports = ({config}) => new Promise((resolve, reject) => {
 			...process.env,
 			// Send this variable to nunjucks-to-html command within the build script.
 			// Make sure to set it relative to cwd.
-			'DONATIONS_BOX_CONFIG_FILE': path.resolve(process.cwd(), config)
+			'DONATIONS_BOX_CONFIG_FILE': configFilepath
 		}
 	}, (error, stdout, stderr) => {
 
 		if (error) { return void reject(error); }
 		if (stderr) { console.error('[donations-box] stderr:', stderr); }
 		if (stdout) { console.log('[donations-box]', stdout); }
+
+		// Store in cache.
+		try { fs.copyFileSync(configFilepath, cachedConfigFilepath); }
+		catch (exception) { console.error('[donations-box][cache]', exception); }
 
 		resolve();
 
